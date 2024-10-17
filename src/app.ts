4c824a53ebf951ae63cf446975060f26941ff44f
Lewis Evans <lewisevansuk@gmail.com>
2024-10-17 17:27:17 +0100

    add logger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
 
+app.use(logger())
 app.get('/up', (c) => c.json({ message: 'ok' }))
 
 export default app
