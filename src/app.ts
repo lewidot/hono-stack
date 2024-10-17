054ff8b1bfadbd7faa73a3b288184d94c2fc85f2
Lewis Evans <lewisevansuk@gmail.com>
2024-10-17 21:56:51 +0100

    serve the react app from the server

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import { Hono } from 'hono'
+import { serveStatic } from 'hono/bun'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 
 const app = new Hono()
 
 app.use(logger())
-app.get('/up', (c) => c.json({ message: 'ok' }))
 
 app.route('/api/expenses', expensesRoute)
+
+app.get('*', serveStatic({ root: './frontend/dist' }))
+app.get('*', serveStatic({ path: './frontend/dist/index.html' }))
 
 export default app
